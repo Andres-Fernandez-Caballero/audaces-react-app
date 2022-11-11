d91c3b60dbc97380aae625366dfb98f592601f80
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2022-11-11 17:49:27 -0300

    ADD folder struct sass

---
+++ src/constants/routes.ts
@@ -0,0 +1,5 @@
+import { ILink } from './../interfaces/ILink';
+export const navBarLinks: ILink[] = [
+	{ name: 'Home', url: '/' },
+	{ name: 'About', url: '/about' },
+];
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './scss/index.scss';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import './index.css';
+import './sass/app.scss';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as popper from '@popperjs/core';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
