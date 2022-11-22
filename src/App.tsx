b44bd780de7e76e2f9c3eccbbfe9cb948a56bf72
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2022-11-22 20:59:10 -0300

    ADD FontAwsome icon pack

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import AppRouter from './routes/AppRouter';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fas, far);
 
 function App(): JSX.Element {
 	return <AppRouter />;
