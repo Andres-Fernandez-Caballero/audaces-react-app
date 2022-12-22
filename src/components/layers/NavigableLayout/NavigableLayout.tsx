389935c94754a80a28d78ee83099534d28094166
Andres Fernandez <andres.fernanezcaballero@davinci.edu.ar>
2022-12-22 20:48:02 -0300

    CREATE NavigableLayout bundle

---
+++ src/components/layers/NavigableLayout/NavigableLayout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+import { navBarLinks } from '../../../constants/routes';
+import OverNavBar from '../../audaces/OverNavBar';
+import Footer from '../../layouts/Footer';
+import Navbar from '../../layouts/Navbar';
+
+export const NavigableLayout: React.FunctionComponent = () => {
+	return (
+		<>
+			<OverNavBar />
+			<Navbar navLinks={navBarLinks} />
+			<Outlet />
+			<Footer />
+		</>
+	);
+};
