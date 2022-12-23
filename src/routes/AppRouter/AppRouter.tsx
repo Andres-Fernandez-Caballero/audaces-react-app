f8298c56962b9d41750518a5535fedbd1a9ac2d2
Andres Fernandez <andres.fernanezcaballero@davinci.edu.ar>
2022-12-22 21:19:19 -0300

    MODIFY router system

--- src/routes/AppRouter/AppRouter.tsx
+++ src/routes/AppRouter/AppRouter.tsx
@@ -1,20 +1,30 @@
 import { Route, Routes } from 'react-router-dom';
+import { NavigableLayout } from '../../components/layers/NavigableLayout/NavigableLayout';
+import { PATH_NAME } from '../../constants/routes';
+import About from '../../pages/About';
 import { Home } from '../../pages/Home';
+import ProductDetail from '../../pages/Products/ProductDetail';
+import ProductList from '../../pages/Products/ProductList';
 
 const AppRouter: React.FC = () => {
 	return (
 		<Routes>
-			<Route path='/' element={<Home />}>
-				<Route path='dashboard' index element={<h1>Dashboard</h1>} />
-				<Route path='settings' element={<h1>Settings</h1>} />
+			<Route path='/' element={<NavigableLayout />}>
+				<Route index element={<Home />} />
+				<Route path={PATH_NAME.ABOUT} element={<About />} />
+
+				<Route path={PATH_NAME.PRODUCTS}>
+					<Route index element={<ProductList />} />
+					<Route path={PATH_NAME.DETAIL} element={<ProductDetail />} />
+				</Route>
+				<Route path='*' element={<h1>404</h1>} />
 			</Route>
-			<Route path='auth' element={<h1>Landing</h1>}>
-				<Route path='landing' element={<h1>LandingPage</h1>} />
+
+			<Route path='auth'>
+				<Route index element={<h1>LandingPage</h1>} />
 				<Route path='login' element={<h1>Login</h1>} />
 				<Route path='register' element={<h1>Register</h1>} />
 			</Route>
-
-			<Route path='*' element={<h1>404</h1>} />
 		</Routes>
 	);
 };
