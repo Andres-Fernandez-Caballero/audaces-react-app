5ed9f55e48d2ef33464eeffb3386a84bfbd0ad4d
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2022-12-28 21:45:31 -0300

    estilado nav en media queries

--- src/components/layouts/Navbar/Navbar.tsx
+++ src/components/layouts/Navbar/Navbar.tsx
@@ -24,41 +24,43 @@ const Navbar: React.FunctionComponent<NavbarProps> = ({
 	return (
 		<div className={styles.navbar}>
 			<nav className='navbar navbar-expand-lg'>
-				<Link className='navbar-brand' to={URL.HOME}>
-					<img
-						className={styles.navbar__logo}
-						src={hidraLogo}
-						height={100}
-						alt='hidra logo'
-					/>
-				</Link>
-				<button
-					className='navbar-toggler'
-					type='button'
-					data-bs-toggle='collapse'
-					data-bs-target='#navbarSupportedContent'
-					aria-controls='navbarSupportedContent'
-					aria-expanded='false'
-					aria-label='Toggle navigation'
-				>
-					<span className='navbar-toggler-icon'></span>
-				</button>
-				<div className='collapse navbar-collapse' id='navbarSupportedContent'>
-					<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
-						{navLinks.map(link => (
-							<li className='nav-item' key={link.name}>
-								<NavLink
-									style={({ isActive }) =>
-										isActive ? activeLinkStyle : inactiveLinkStyle
-									}
-									className={styles.navbar__item}
-									to={link.url}
-								>
-									{link.name}
-								</NavLink>
-							</li>
-						))}
-					</ul>
+				<div className='container-fluid'>
+					<Link className='navbar-brand' to={URL.HOME}>
+						<img
+							// className={styles.navbar__logo}
+							src={hidraLogo}
+							height={100}
+							alt='hidra logo'
+						/>
+					</Link>
+					<button
+						className='navbar-toggler'
+						type='button'
+						data-bs-toggle='collapse'
+						data-bs-target='#navbarSupportedContent'
+						aria-controls='navbarSupportedContent'
+						aria-expanded='false'
+						aria-label='Toggle navigation'
+					>
+						<span className='navbar-toggler-icon'></span>
+					</button>
+					<div className='collapse navbar-collapse' id='navbarSupportedContent'>
+						<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
+							{navLinks.map(link => (
+								<li className='nav-item' key={link.name}>
+									<NavLink
+										style={({ isActive }) =>
+											isActive ? activeLinkStyle : inactiveLinkStyle
+										}
+										className={styles.navbar__item}
+										to={link.url}
+									>
+										{link.name}
+									</NavLink>
+								</li>
+							))}
+						</ul>
+					</div>
 					<div className={styles.navbar__cartPosition}>
 						<div className={styles.navbar__cartshop}>
 							<FontAwesomeIcon icon='cart-shopping' />
