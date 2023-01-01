211a47900141649f7ba43b795e84fbdd2b6cbbca
Andres Fernandez <andres.fernandezcaballero@davinci.edu.ar>
2023-01-01 17:28:28 -0300

    MODIFY position cart layout

--- src/components/layouts/Navbar/Navbar.tsx
+++ src/components/layouts/Navbar/Navbar.tsx
@@ -39,6 +39,11 @@ const Navbar: React.FunctionComponent<NavbarProps> = ({
 					>
 						<span className='navbar-toggler-icon'></span>
 					</button>
+					<div className={styles.navbar__cartPosition}>
+						<div className={styles.navbar__cartshop}>
+							<FontAwesomeIcon icon='cart-shopping' />
+						</div>
+					</div>
 					<div className='collapse navbar-collapse' id='navbarSupportedContent'>
 						<ul className='navbar-nav me-auto mb-2 mb-lg-0'>
 							{navLinks.map(link => (
@@ -55,11 +60,6 @@ const Navbar: React.FunctionComponent<NavbarProps> = ({
 								</li>
 							))}
 						</ul>
-					</div>
-					<div className={styles.navbar__cartPosition}>
-						<div className={styles.navbar__cartshop}>
-							<FontAwesomeIcon icon='cart-shopping' />
-						</div>
 					</div>
 				</div>
 			</nav>
