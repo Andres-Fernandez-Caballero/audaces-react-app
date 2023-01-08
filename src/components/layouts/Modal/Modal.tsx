4b036930abac298effe51a497dcb93b437779a34
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2023-01-08 20:45:33 -0300

    cambio en la importacion

--- src/components/layouts/Modal/Modal.tsx
+++ src/components/layouts/Modal/Modal.tsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 import { Modal } from 'react-responsive-modal';
 import 'react-responsive-modal/styles.css';
-import Tabs from '../Tabs';
+import Tabs from '@components/layouts/Tabs';
 
 const Modals: React.FC = () => {
 	const [open, setOpen] = useState(false);
@@ -11,12 +11,20 @@ const Modals: React.FC = () => {
 
 	const onOpenModal = (): void => setOpen(true);
 
+	const closeIcon = (
+		<FontAwesomeIcon
+			icon='xmark'
+			className='text-info'
+			style={{ fontSize: '2rem' }}
+		/>
+	);
+
 	return (
 		<>
 			<button className='btn btn-primary' onClick={onOpenModal}>
 				<FontAwesomeIcon icon='user' />
 			</button>
-			<Modal open={open} onClose={onCloseModal} center>
+			<Modal open={open} onClose={onCloseModal} center closeIcon={closeIcon}>
 				<Tabs />
 			</Modal>
 		</>
